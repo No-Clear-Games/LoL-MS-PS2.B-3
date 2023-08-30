5bbcb0ae8d7b8b739fe5f46177ceca009e1a2414
AhmadReza <a.reza.sanatjoo@gmail.com>
2023-08-30 15:17:36 +0330

    Add train passengers

--- Assets/Scripts/Cart.cs
+++ Assets/Scripts/Cart.cs
@@ -3,9 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Cart : MonoBehaviour
 {
+    [SerializeField] private GameObject[] people;
+    [SerializeField] private Transform[] seats;
+    
+
+    private void Start()
+    {
+        foreach (Transform seat in seats)
+        {
+            AddPassenger(seat);
+        }
+    }
+
+    private void AddPassenger(Transform seat)
+    {
+        int index = Random.Range(0, people.Length);
+        GameObject passenger = Instantiate(people[index], seat);
+        Animator animator = passenger.GetComponent<Animator>();
+
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("StationEnter"))
